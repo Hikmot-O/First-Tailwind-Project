c8a3c6e96f4106f4246fe591b6a05d96a087d6c7
Hikmot <69507776+Hikmot-O@users.noreply.github.com>
2022-11-12 16:34:35 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div>
+        <p>growing with data analytics</p>
+        <h1>Grow with data.</h1>
+
+        <div>Fast, flexible financing for</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
