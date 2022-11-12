edcca70232f2123651b2726ea945b4bd4033ecd4
Hikmot-O <hikmotokunola@gmail.com>
2022-11-12 23:30:29 +0100

    Worked on hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className='text-white'>
-      <div>
-        <p>growing with data analytics</p>
-        <h1>Grow with data.</h1>
+      <div className='max-w-[800px] h-screen w-full mt-[-96px] flex justify-center flex-col text-center items-center mx-auto '>
+        <p className='p-2 text-[#00df9a] font-bold uppercase'>growing with data analytics</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold'>Grow with data.</h1>
 
-        <div>Fast, flexible financing for</div>
+        <div>
+        <p className='md:text-5xl sm:text-3xl text-xl font-bold'>Fast, flexible financing for</p>
+        </div>
       </div>
     </div>
   )
